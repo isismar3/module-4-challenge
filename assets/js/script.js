a696b57177ca65621a80d9fe8fc79caae057ba1b
Isis Mar <isismar@Isiss-MacBook-Air.local>
2024-01-12 19:13:22 -0500

    drafted module 4 challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,17 @@
 
 
 
-let startButton = document.getElementById("startButton")
+let startButton = document.getElementById("start-button")
 let questionsSection = document.getElementById("questionsSection")
 let menuContainer = document.getElementById("menu")
+let score;
+let nextQuestion;
+let i = 0;
+let timerInterval;
+const timerEl = document.getElementById("timer")
+const questionDiv = document.getElementById("questions")
+
+
 
 const questions = [
     {
@@ -33,9 +41,7 @@ const questions = [
             "green", "purple", "brown", "blue",
         ],
         answer: 2
-    },
-
-
+    }
 ]
 
 function startMenu(){
@@ -43,7 +49,7 @@ function startMenu(){
     if(show === "visible") {
         menuContainer.setAttribute("data-startMenu", "none");
         menuContainer.setAttribute("style", "display: none;");
-        // generateQuestions()
+        startQuiz(questions)
     }
     else{
         menuContainer.setAttribute("data-startMenu", "visible");
@@ -51,11 +57,93 @@ function startMenu(){
     }
 
     }
-
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
     startMenu()
-
+    timerInterval = setInterval(updateTimer, 1000);
 })
 
+function startQuiz(item) {
+    let score = 0;
+
+        const content = item[i]; 
+        if (content) {
+        const question = content.question;
+        const answer = content.answer;
+        const choices = content.choices;
+    //     console.log(choices);
+    //     console.log(answer);
+    //     console.log(content);
+    //    console.log(question);
+        const divEl = document.getElementById("questions")
+        const questionEl = document.createElement("h2")
+        questionEl.setAttribute("class", "previous");
+        questionEl.textContent = question;
+        divEl.append(questionEl);
+        for (let index = 0; index < choices.length; index++) {
+            const buttonEl = document.createElement("button");
+            buttonEl.textContent = choices[index];
+            buttonEl.value = index;
+            const breakEl = document.createElement("br");
+            buttonEl.setAttribute("class", "previous");
+            breakEl.setAttribute("class", "previous");
+            divEl.append(buttonEl);
+            divEl.append(breakEl);
+
+        }}
+    };
+    questionDiv.addEventListener("click", function(event) {
+        event.preventDefault();
+        if (event.target.tagName === "BUTTON")
+        { i++
+            const answer = questions[i - 1].answer
+            console.log(answer);
+            if (parseInt(event.target.value) === answer) {
+            const correct = document.getElementById("correct");
+            correct.textContent = "correct"
+            }
+            else {
+                const correct = document.getElementById("correct");
+                correct.textContent = "wrong"
+            }
+            display()
+            startQuiz(questions);
+        }
+    })
+    function display() {
+        const previous = document.querySelectorAll(".previous")
+        for (let index = 0; index < previous.length; index++){
+            previous[index].style.display = "none"
+        }
+    }
+
+
+
+const targetTimeInSeconds = 1 * 60;
+
+let remainingTime = targetTimeInSeconds;
+function updateTimer() {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+    timerEl.textContent = `${minutes}:${seconds}`
+    console.log(`${minutes}:${seconds}`);
+    
+    // If the timer reaches zero, you can perform any action here
+    if (remainingTime === 0) {
+
+        console.log("Timer expired!");
+        clearInterval(timerInterval);
+    }
+    else if(i  === questions.length) {
+        console.log("finish question");
+        clearInterval(timerInterval);
+        const correct = document.getElementById("correct");
+            correct.textContent = "Quiz Finished!"
+    }
+
+
+    remainingTime--;
+}
+
+// Update the timer every second
 
